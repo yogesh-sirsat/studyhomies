31f506474d6404a2d459065dd0252f6b1a4964de
Yogesh Sirsat <yogeshsirsat56@gmail.com>
2023-11-12 23:11:59 +0530

    Add domain urls in cors origin.

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "https://localhost:3000",
-  process.env.FRONTEND_URL,
+  "https://studyhomies.com",
+  "https://www.studyhomies.com",
 ];
 app.use(
   cors({
