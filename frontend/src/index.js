aae78048d121e382af449188f9ac23b327accbc2
Yogesh Sirsat <yogeshsirsat56@gmail.com>
2023-11-12 23:40:51 +0530

    Remove console.log messages in production.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,12 @@ import ChatScreen from './components/ChatScreen';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+if (process.env.NODE_ENV === 'production') {
+  console.log = () => {}
+  console.error = () => {}
+  console.debug = () => {}
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
